3e7586d809a7194ba8109fa3cd6c372cd160d67f
Fabri19f <104957036+Fabri19f@users.noreply.github.com>
2022-06-06 18:43:11 -0400

    Add files via upload

--- my-grid- system-v0.1/components.js
+++ my-grid- system-v0.1/components.js
@@ -1,6 +1,8 @@
 let navButton = document.getElementById("navButton");
 let navMenu = document.getElementById("navMenu");
+let body = document.querySelector("body");
 
 navButton.addEventListener("click", ()=>{
     navMenu.classList.toggle("navbar-menu-show");
+    body.classList.toggle("lock-scroll-body");
 });
